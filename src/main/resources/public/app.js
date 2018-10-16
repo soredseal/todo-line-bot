ccb33634dd38f35c6c20676da6f170745e495568
Natkrita Chumlek <noon.nq@MacBook-Pro.local>
2018-10-16 15:34:12 +0700

    add todo edit page

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -2,12 +2,102 @@ window.onload = function (e) {
     liff.init(function (data) {
         initializeApp(data);
     });
-    // initializeApp();
 };
 
 function initializeApp(data) {
-    let url = '/api/list/' + data.context.userId;
-    // let url = '/api/list/' + 'U2d46bcd5a990b1ba8528829eae526238';
+    list(data.context.userId);
+}
+
+
+function renderTasks(list) {
+    let container = document.getElementById('container');
+    container.innerHTML = null;
+
+    list.forEach((todo, i) => {
+        let taskContainer = document.createElement('div');
+        let taskContainerClassList = taskContainer.classList;
+        taskContainerClassList.add('task');
+        if (todo.completed === true) {
+            taskContainerClassList.add('strike');
+        }
+
+        let completeContainer = document.createElement('div');
+        let completeContainerClassList = completeContainer.classList;
+        completeContainerClassList.add('checkbox-container', 'complete-mark');
+
+        let completeWrapper = document.createElement('label');
+        completeWrapper.className = 'checkbox-wrapper';
+
+        let complete = document.createElement('input');
+        complete.setAttribute('type', 'checkbox');
+        complete.setAttribute('value', i);
+        if (todo.completed === true) {
+            complete.setAttribute('checked', 'true');
+        }
+        complete.addEventListener('change', function(e) {
+            let taskId = todo.id;
+            let userId = todo.user;
+            let flag = e.target.checked;
+            markComplete(userId, taskId, flag);
+        });
+
+        let completeMark = document.createElement('span');
+        completeMark.className = 'checkbox-mark';
+        completeWrapper.appendChild(complete);
+        completeWrapper.appendChild(completeMark);
+        completeContainer.appendChild(completeWrapper);
+
+
+        let task = document.createElement('p');
+        let taskClassList = task.classList;
+        taskClassList.add('task');
+        task.innerText = todo.task;
+
+
+        let time = document.createElement('p');
+        let text = todo.time;
+        time.className = 'task';
+        text = text.replace('T', ' ');
+        time.innerText = text;
+
+
+        let pinContainer = document.createElement('div');
+        let pinContainerClassList = pinContainer.classList;
+        pinContainerClassList.add('checkbox-container', 'pin-mark');
+
+        let pinWrapper = document.createElement('label');
+        pinWrapper.className = 'checkbox-wrapper';
+
+        let pin = document.createElement('input');
+        pin.setAttribute('type', 'checkbox');
+        pin.setAttribute('value', i);
+        if (todo.important === true) {
+            pin.setAttribute('checked', 'true');
+        }
+        pin.addEventListener('change', function(e) {
+            let taskId = todo.id;
+            let userId = todo.user;
+            let flag = e.target.checked;
+            markImportant(userId, taskId, flag);
+        });
+
+        let pinMark = document.createElement('span');
+        pinMark.className = 'checkbox-mark';
+        pinWrapper.appendChild(pin);
+        pinWrapper.appendChild(pinMark);
+        pinContainer.appendChild(pinWrapper);
+
+
+        taskContainer.appendChild(completeContainer);
+        taskContainer.appendChild(task);
+        taskContainer.appendChild(time);
+        taskContainer.appendChild(pinContainer);
+        container.appendChild(taskContainer);
+    })
+}
+
+function list(userId) {
+    let url = '/api/list/' + userId;
     fetch(url)
         .then(
             function(response) {
@@ -17,7 +107,6 @@ function initializeApp(data) {
                     return;
                 }
 
-                // Examine the text in the response
                 response.json().then(function(resp) {
                     console.log(resp);
                     renderTasks(resp);
@@ -27,68 +116,64 @@ function initializeApp(data) {
         .catch(function(err) {
             console.log('Fetch Error :-S', err);
         });
-
-
-    // closeWindow call
-    // document.getElementById('closewindowbutton').addEventListener('click', function () {
-    //     liff.closeWindow();
-    // });
-
-    // // sendMessages call
-    // document.getElementById('sendmessagebutton').addEventListener('click', function () {
-    //     liff.sendMessages([{
-    //         type: 'text',
-    //         text: "You've successfully sent a message! Hooray!"
-    //     }, {
-    //         type: 'sticker',
-    //         packageId: '2',
-    //         stickerId: '144'
-    //     }]).then(function () {
-    //         window.alert("Message sent");
-    //     }).catch(function (error) {
-    //         window.alert("Error sending message: " + error);
-    //     });
-    // });
-
-    //get profile call
-    // document.getElementById('getprofilebutton').addEventListener('click', function () {
-    //     liff.getProfile().then(function (profile) {
-    //         document.getElementById('useridprofilefield').textContent = profile.userId;
-    //         document.getElementById('displaynamefield').textContent = profile.displayName;
-    //
-    //         var profilePictureDiv = document.getElementById('profilepicturediv');
-    //         if (profilePictureDiv.firstElementChild) {
-    //             profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-    //         }
-    //         var img = document.createElement('img');
-    //         img.src = profile.pictureUrl;
-    //         img.alt = "Profile Picture";
-    //         profilePictureDiv.appendChild(img);
-    //
-    //         document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-    //         toggleProfileData();
-    //     }).catch(function (error) {
-    //         window.alert("Error getting profile: " + error);
-    //     });
-    // });
 }
 
+function markComplete(userId, taskId, flag) {
+    let url = '/api/completed/' + userId;
+    let payload = {
+        id: taskId,
+        flag: flag
+    };
 
-function renderTasks(list) {
-    let container = document.getElementById('container');
-    list.forEach(data => {
-        let el = document.createElement('div');
-        el.className = 'task';
-        el.innerText = data;
-        container.appendChild(el)
+    fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify(payload),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(function(response){
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+            }
+
+            response.json().then(function(resp) {
+                renderTasks(resp);
+            });
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function markImportant(userId, taskId, flag) {
+    let url = '/api/important/' + userId;
+    let payload = {
+        id: taskId,
+        flag: flag
+    };
+
+    fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(payload),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
     })
-}
+        .then(function(response){
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+            }
 
-// function toggleProfileData() {
-//     var elem = document.getElementById('profileinfo');
-//     if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-//         elem.style.display = "none";
-//     } else {
-//         elem.style.display = "block";
-//     }
-// }
+            response.json().then(function(resp) {
+                renderTasks(resp);
+            });
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
